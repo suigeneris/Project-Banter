238595cd79620e8dc1e0054dbb25d6e952222532
Jonathan Aguele <SuiGeneris@jonathans-imac.home>
2014-07-03 12:03:45 +0100

    Initial Commit

---
+++ Banter/BanterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BanterAppDelegate.h
+//  Banter
+//
+//  Created by Jonathan Aguele on 03/07/2014.
+//  Copyright (c) 2014 Sui Generis Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BanterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Banter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Banter
+//
+//  Created by Jonathan Aguele on 03/07/2014.
+//  Copyright (c) 2014 Sui Generis Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BanterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BanterAppDelegate class]));
+    }
+}
